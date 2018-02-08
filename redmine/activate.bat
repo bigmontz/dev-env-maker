d12152ac8ff80c3be46421969e775515670dbed7
Jonathan Rangel Porto <jonathan.porto@radixeng.com.br>
2018-02-08 19:00:04 -0200

    Fixing enviroment variables configuration #1

--- redmine/activate.bat
+++ redmine/activate.bat
@@ -5,16 +5,14 @@ IF EXIST %~dp0 SETX /M PATH "%~dp0;%PATH%"
 ECHO %REDMINE_URL%
 ECHO %REDMINE_APIKEY%
 
-GOTO FIM
-
 IF "%REDMINE_URL%"=="" (
     SET /p redmine_url="Set your redmine url with procotol (Example: http://redmine.com):"
-    SETX REDMINE_URL %redmine_url%
+    SETX REDMINE_URL "%redmine_url%"
 )
 
 IF "%REDMINE_APIKEY%"=="" (
     SET /p redmine_apikey="Set your redmine url api key:"
-    SETX REDMINE_APIKEY %redmine_apikey%
+    SETX REDMINE_APIKEY "%redmine_apikey%"
 )
 
 
